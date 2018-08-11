19d5be3465bc4ce3a6f7f4444f00540c068a7024
Kelly Garry <kellygarry@KG-Macbook-Air.local>
2018-08-10 22:30:01 -0600

    checkpoint 3 create HTTP server

---
+++ simple-http-node-server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const port = 3000;
+
+// Handles HTTP requests.
+const requestHandler = (request, response) => {
+  response.end(`Handling a request on port ${port}`)
+};
+
+// Create a server and pass in the  requestHandler function
+const server = http.createServer(requestHandler);
+
+// Start the server listening on part 3 000
+server.listen(port, (err) => {
+  if (err) {
+    return console.log(`You have an error:  ${err}`);
+  }
+
+  console.log(`server is listening on ${port}`);
+});
+
+fs.writeFile('hello-world'.txt', 'Hello to this great world', 'utf8', (err) => {
+  if (err) throw err;
+
+  console.log('success')
+});
